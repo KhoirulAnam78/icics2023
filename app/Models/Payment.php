501a214196efc54d0dc9d7d819e85ac1abaf0e1d
Khoirul Anam <khoirulanam4580@gmail.com>
2023-08-06 22:23:49 +0700

    setup fitur payment

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -5,14 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class UploadAbstract extends Model
+class Payment extends Model
 {
     use HasFactory;
     protected $guarded = ['id'];
-    public function participant()
-    {
-        return $this->belongsTo(Participant::class);
-    }
 
     public function detailPayments()
     {
