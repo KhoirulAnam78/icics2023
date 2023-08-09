51b9968a428e3366034565676d0cbea61e76a6d3
Khoirul Anam <khoirulanam4580@gmail.com>
2023-08-09 09:52:42 +0700

    update dashboard participant

--- resources/views/participant/dashboard.blade.php
+++ resources/views/participant/dashboard.blade.php
@@ -4,20 +4,22 @@
     <div class="row mx-3">
         @if (Auth::user()->participant->participant_type !== 'participant')
             @if (Auth::user()->participant->uploadAbstracts->first())
-                <p><strong>Information : </strong>Your abstract status is
+                <p><strong>Abstract Information : </strong>Your abstract status is
                     <strong>{{ Auth::user()->participant->uploadAbstracts->first()->status }}</strong>
+                    <br>
                 </p>
             @else
                 <p><strong>Information : </strong>You haven't added abstract yet, add abstract in abstract menu</p>
             @endif
+        @endif
+    </div>
+    <div class="row mx-3">
+        @if (Auth::user()->participant->payments->first())
+            <p><strong>Payment Information : </strong>Your payment status is
+                <strong>{{ Auth::user()->participant->payments->first()->validation }}</strong>
+            </p>
         @else
-            @if (Auth::user()->participant->payments->first())
-                <p><strong>Information : </strong>Your payment status is
-                    <strong>{{ Auth::user()->participant->payments->first()->validation }}</strong>
-                </p>
-            @else
-                <p><strong>Information : </strong>you have not made a payment, add payment in payment menu</p>
-            @endif
+            <p><strong>Information : </strong>you have not made a payment, add payment in payment menu</p>
         @endif
 
     </div>
