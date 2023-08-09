141ba789a3a244fc556130330033b304ec18efbc
Khoirul Anam <khoirulanam4580@gmail.com>
2023-08-09 23:22:23 +0700

    update style + header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,4 +1,4 @@
-    @if (Auth::user())
+    {{-- @if (Auth::user())
         <div class="container-fluid">
             <div class="row justify-content-end p-3" style="background-color: #0366fc">
                 <a href="/dashboard" class="primary-btn">{{ Auth::user()->email }}</a>
@@ -11,7 +11,7 @@
                 <a href="/register" class="primary-btn mx-3">Registration</a>
             </div>
         </div>
-    @endif
+    @endif --}}
     <!-- Header Section Begin -->
     <header class="header-section">
 
@@ -19,36 +19,50 @@
             <div class="logo">
                 <a href="./index.html">
                     {{-- <h2>ICICS2023</h2> --}}
-                    <img src="{{ url('') }}/assets/img/logo-icics2023.png" alt="">
+                    <img src="{{ url('') }}/assets/img/logo-icics2023.png" alt="logo.png">
                 </a>
             </div>
             <div class="nav-menu">
                 <nav class="mainmenu mobile-menu">
                     <ul>
                         <li class="{{ $title == 'Home' ? 'active' : '' }}"><a href="/">Home</a></li>
-                        <li class="{{ $title == 'Registration' ? 'active' : '' }}"><a href="/register">Registration</a>
-                            <ul class="dropdown">
-                                <li><a href="#">Registration Information</a></li>
-                                <li><a href="/register">Registration Conference</a></li>
-                            </ul>
-                        </li>
-                        <li class="{{ $title == 'About' ? 'active' : '' }}"><a href="./speaker.html">About</a>
-                            <ul class="dropdown">
+                        <li class="{{ $title == 'Information' ? 'active' : '' }}"> <a href="#">Information</a>
+                            <ul class="dropdown" style="width:300px">
+                                <li><a href="#">Registration fee</a></li>
+                                <li><a href="#">Scientific Committee</a></li>
+                                <li><a href="#">Steering Committee</a></li>
+                                <li><a href="#">Organizing Committee</a></li>
                                 <li><a href="/about-conference">About Conference</a></li>
                                 <li><a href="#">Contacts</a></li>
                             </ul>
                         </li>
-                        <li class="{{ $title == 'Speakers' ? 'active' : '' }}"><a href="./about-us.html">Speakers</a>
                         </li>
                         {{-- <li class="{{ $title == 'Presentations' ? 'active' : '' }}"><a
                             href="./about-us.html">Presentations</a></li> --}}
                         <li class="{{ $title == 'ICICS2023 Rundown' ? 'active' : '' }}"><a
                                 href="/rundown-icics2023">ICICS2023
                                 Rundown</a></li>
+
+                        <li class="{{ $title == 'Satellite Event' ? 'active' : '' }}"><a href="#">Satellite
+                                Event</a>
+                            <ul class="dropdown" style="width:300px">
+                                <li><a href="#">FGD MBKM</a></li>
+                                <li><a href="#">FGD Akreditasi Internasional</a></li>
+                                <li><a href="#">Kongres HKI</a></li>
+                                <li><a href="#">Forum Ketua Jurusan Kimia</a></li>
+                                <li><a href="#">Field Trip</a></li>
+                            </ul>
+                        </li>
                         <li class="{{ $title == 'Download' ? 'active' : '' }}"><a href="./about-us.html">Download</a>
                         </li>
                     </ul>
                 </nav>
+                @if (Auth::user())
+                    <a href="/dashboard" class="primary-btn">{{ Auth::user()->email }}</a>
+                @else
+                    <a href="/login" class="primary-btn">Login</a>
+                    <a href="/register" class="primary-btn mx-3">Registration</a>
+                @endif
             </div>
             <div id="mobile-menu-wrap"></div>
         </div>
