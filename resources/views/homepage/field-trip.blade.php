eb96a4d741316a1d47282a54d415a9ee2544a4a0
Khoirul Anam <khoirulanam4580@gmail.com>
2023-08-10 16:23:46 +0700

    field trip

---
+++ resources/views/homepage/field-trip.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.main')
+
+@section('content')
+    <!-- Contact Form Section Begin -->
+    <section class="contact-from-section spad" style="padding-top:0px">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="section-title">
+                        <h2>Field Trip</h2>
+                        {{-- <p>Fill in the form below to register.</p> --}}
+                    </div>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-lg-12">
+                    {{-- CONTENT --}}
+                    <div class="col-6">
+                        <div class="card">
+                            <img class="card-img-top"
+                                src="https://www.tempatwisata.pro/media/uploads/3066/2d3bfe9359b694f25a0b6d12eb7e9136.jpg"
+                                alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the
+                                    bulk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
