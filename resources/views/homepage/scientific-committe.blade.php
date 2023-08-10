d009cd9b3045855b0aa90670a4cff8351b59aea7
anugrahmukti <anugrahmukti7@gmail.com>
2023-08-10 16:44:31 +0700

    committe done

--- resources/views/homepage/scientific-committe.blade.php
+++ resources/views/homepage/scientific-committe.blade.php
@@ -17,6 +17,45 @@
                     <div class="card">
                         <div class="card-body">
                             {{-- CONTENT --}}
+                            <div class="row justify-content-center">
+                                {{-- Pembatas --}}
+                                <div style="padding-left: 10px; padding-right: 10px">
+                                    <div class="card" style="width: 16rem; border: 2px solid black">
+                                        <img style="height: 280px"
+                                            src="{{ url('') }}/assets/img/scientific-committe/SC-1.jpg"
+                                            class="card-img-top" alt="...">
+                                        <div class="card-body" style="height: 200px">
+                                            <h5 class="card-title">Prof. Drs. Sutrisno, M.Sc., Ph.D</h5>
+                                            <p class="card-text">Department of Chemistry, Universitas Jambi</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                {{-- Pembatas --}}
+                                <div style="padding-left: 10px; padding-right: 10px">
+                                    <div class="card" style="width: 16rem; border: 2px solid black">
+                                        <img style="height: 280px"
+                                            src="{{ url('') }}/assets/img/scientific-committe/SC-2.png"
+                                            class="card-img-top" alt="...">
+                                        <div class="card-body" style="height: 200px">
+                                            <h5 class="card-title">Dr. Sukro Muhab, M.Si</h5>
+                                            <p class="card-text">Department of Educational Chemistry, Universitas Negeri
+                                                Jakarta</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                {{-- Pembatas --}}
+                                <div style="padding-left: 10px; padding-right: 10px">
+                                    <div class="card" style="width: 16rem; border: 2px solid black">
+                                        <img style="height: 280px"
+                                            src="{{ url('') }}/assets/img/scientific-committe/SC-3.png"
+                                            class="card-img-top" alt="...">
+                                        <div class="card-body" style="height: 200px">
+                                            <h5 class="card-title">Muhamad A. Martoprawiro, M.S., Ph.D. </h5>
+                                            <p class="card-text">Department of Chemistry, Institut Teknologi Bandung</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
 
                         </div>
                     </div>
